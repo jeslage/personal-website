f36b033e9ce5de1c0d48a2f0a0483a1bdc0ad579
Johannes Eslage <hello@johanneseslage.de>
2020-02-24 23:00:15 +0100

    Updated projects

--- src/contexts/themeProvider.js
+++ src/contexts/themeProvider.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ThemeProvider as StyledComponentsThemeProvider } from "styled-components";
 
 export const ThemeContext = React.createContext();
